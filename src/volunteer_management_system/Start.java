05b34630234a81822c8bac1f1e32f67fbe0110e9
aarya095 <aaryasarfare28@gmail.com>
2024-09-21 14:26:31 +0530

    Update Start.java

--- src/volunteer_management_system/Start.java
+++ src/volunteer_management_system/Start.java
@@ -11,7 +11,7 @@ public class Start extends JFrame implements ActionListener {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         
-        JLabel heading = new JLabel("VOLUNTEER MANAGEMET SYSTEM");
+        JLabel heading = new JLabel("VOLUNTEER MANAGEMENT SYSTEM");
         heading.setBounds(50, 30, 1200, 60);
         heading.setFont(new Font("serif", Font.PLAIN, 60));
         heading.setForeground(Color.BLACK);
