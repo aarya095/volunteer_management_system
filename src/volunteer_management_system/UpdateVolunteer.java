43950afc6fe6d4945b6faca131a4b94287c7a862
aarya095 <aaryasarfare28@gmail.com>
2024-09-22 15:43:59 +0530

    problem fix -2

--- src/volunteer_management_system/UpdateVolunteer.java
+++ src/volunteer_management_system/UpdateVolunteer.java
@@ -14,7 +14,7 @@ import java.sql.*;
 public class UpdateVolunteer extends JFrame implements ActionListener {
 	
 	
-	JTextField tfeducation,tffname,tfphone,tfemail,tfaadhar,tfdob,tfaddress,tfdesignation;
+	JTextField tfeducation,tffname,tfphone,tfemail,tfaddress,tfdesignation;
 	JButton add,back;
 	JLabel lblvolunteerId;
 	String volunteerId;
